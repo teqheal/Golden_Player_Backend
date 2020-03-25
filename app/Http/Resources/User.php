0c2bd0db701bcd36357a835ac9f47246173af9e5
Ravi Gaudani <ravi.b.gaudani@gmail.com>
2020-03-25 17:28:45 +0530

    solve date format issue

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -18,6 +18,7 @@ class User extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'email_verified_at' => $this->email_verified_at->format('d/m/Y H:i:s'),
             'birth_date' => $this->birth_date->format('d/m/Y'),
             'profile_pic' =>$this->profile_pic
         ];
--- app/User.php
+++ app/User.php
@@ -37,4 +37,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $dates = ['birth_date'];
 }
