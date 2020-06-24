a9018b84656de65bece63800128918b99cf31951
Ravi Gaudani <ravi.b.gaudani@gmail.com>
2020-06-24 14:32:59 +0530

    meet celeb request

--- app/MeetCelebRequest.php
+++ app/MeetCelebRequest.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class MeetCelebRequest extends Model
 {
-    //
+    protected $table = 'meet_celeb_requests';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'player_id',
+        'celebrity_id',
+        'payment_id',
+        'created_at',
+        'updated_at'
+    ];
 }
